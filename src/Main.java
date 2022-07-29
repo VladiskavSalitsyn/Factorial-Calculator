25f640afa525a3c961db96822e2d1266416fb50b
Vladislav Salitsyn <salitsyn95@mail.ru>
2022-07-29 23:38:26 +0400

    'commit1'

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+        String Factorial = value + "!" + " = ";
+        int n = value;
+
+        for (int i = 1; i <= n; i = i + 1) {
+            value = value * i;
+            System.out.println(Factorial + value);
+            }
+        }
+    }
+
